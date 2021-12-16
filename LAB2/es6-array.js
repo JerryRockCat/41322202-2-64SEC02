2198426cdfe5dc469d84382b987f0596bce3325a
PlachonAiAi <630112418052@bru.ac.th>
2021-12-16 18:23:02 +0700

    lab6

---
+++ LAB2/es6-array.js
@@ -0,0 +1,72 @@
+let cars = ["Toyota", "Honda", "Mazda"];
+let fruits = [];
+fruits[0] = "Applr";
+fruits[3] = "Mango";
+let rname = new Array("Win", "John", "James");
+console.log(rname);
+
+let popCar = cars.pop();
+console.log(popCar);
+console.log(cars);
+
+cars.unshift("Kubota");
+console.log(cars);
+cars.shift();
+console.log(cars);
+
+cars.splice(1,0,"Honda", "Kubota");
+console.log(cars);
+
+const carSlide = cars.slice(1,2)
+console.log(carSlide);
+
+const newArr = cars.concat(rname);
+console.log(newArr);
+const newArr2 = [...cars, ...rname]; //spread operator
+console.log(newArr2);
+
+console.log(newArr.toString());
+
+//itteration
+let Text = "";
+for(value of rname){
+    Text += value + ";";
+}
+console.log(Text);
+
+const numbers = [10, 20, 30];
+numbers.forEach(addNum);
+
+function addNum(value, index, array){
+  console.log(value + 2);
+}
+
+const ns = numbers.map(addNumMap);
+console.log(ns);
+function addNumMap(value){
+    return value * 2;
+}
+
+const over10 = numbers.filter(over10fn)
+console.log(over10);
+function over10fn(value) {
+    return value > 10;
+}
+
+const reOver10 = numbers.reduce(over10fn)
+console.log(reOver10);
+
+const everOver10 = numbers.every(over10fn)
+console.log(reOver10);
+const someOver10 = numbers.some(over10fn)
+console.log(reOver10);
+
+let resFind = numbers.find(over10fn)
+console.log(resFind);
+let resFindIndex = numbers.indexOf(over10fn)
+console.log(resFindIndex);
+
+let alphab = Array.from("ABCDefG021");
+console.log(alphab)
+
+
